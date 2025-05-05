411b6e11246e3aa755ecfea25fe5722df8c09b0f
leodoneDev <leodone125@gmail.com>
2025-05-06 03:03:30 +0700

    register backend

---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,39 @@
+import { prisma} from '@/lib/prisma';
+import bcrypt from 'bcrypt';
+
+export async function POST(req: Request) {
+  try {
+    const { name, email, phoneNumber, password, role } = await req.json();
+
+    // Check for existing user
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+    if (existingUser) {
+      return new Response(JSON.stringify({ error: true, message: 'User already exists' }), {
+        
+      });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create new user
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        phoneNumber,
+        password: hashedPassword,
+        role: role,
+        isVerified: false,
+        isApproved: false,
+      },
+    });
+
+    return new Response(JSON.stringify({ error: false,  message: 'User registered Successfully!', user }), {
+      status: 201,
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: true, message: 'Server not found. Please try again.' }), {
+    });
+  }
+}
