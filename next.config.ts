9bdd63102aec3e5f4b530203fc50bdb555dbc007
Vong <arncy65@gmail.com>
2025-06-04 20:06:42 +0700

    Update config sub folder.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+	basePath: "/cp", 
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
