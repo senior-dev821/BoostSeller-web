464b92a2b620a65cac09cfe15052004351be5c86
leodoneDev <leodone125@gmail.com>
2025-06-23 00:53:25 +0700

    dashboard update

--- src/app/api/admin/statistics/worktraffic/route.ts
+++ src/app/api/admin/statistics/worktraffic/route.ts
@@ -2,6 +2,7 @@
 import { NextRequest, NextResponse } from "next/server"
 import { PrismaClient } from '@prisma/client';
 import { eachDayOfInterval, format, getHours } from "date-fns"
+import { getUserFromToken } from '@/lib/auth';
 
 const prisma = new PrismaClient();
 
@@ -9,6 +10,34 @@ export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url)
   const from = new Date(searchParams.get("from") || "")
   const to = new Date(searchParams.get("to") || "")
+
+  const currentUser = await getUserFromToken();
+  
+  if (!currentUser) {
+  return NextResponse.json({ error: true, message: "Unauthorized" }, { status: 401 });
+  }
+
+  const hostesses = await prisma.hostess.findMany({
+      where: {
+      adminId: currentUser.id,
+      },
+      select: {
+      id: true,
+      },
+  });
+
+  const hostessIds = hostesses.map(h => h.id);
+
+  const interests = await prisma.interest.findMany({
+    where: {
+      adminId: currentUser.id,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  const interestIds = interests.map(interest => interest.id);
 
   // 1. Heatmap Data (Sun–Sat, working hours 8–19)
 	const leads = await prisma.lead.findMany({
@@ -43,13 +72,23 @@ export async function GET(req: NextRequest) {
 
 
   // 2. Group Status (availability)
-  const performers = await prisma.performer.findMany()
+  const performers = await prisma.performer.findMany({
+    where: {
+      adminId: currentUser.id,
+    },
+  })
   const availablePerformers = performers.filter(
     (p) =>
       p.available &&
       p.acceptedCount - (p.closedCount + p.completedCount) < p.assignedCount
   )
-  const allGroups = await prisma.group.findMany()
+  const allGroups = await prisma.group.findMany({
+    where: {
+      interestId: {
+        in: interestIds,
+      }
+    },
+  })
 
   const groupStatus = allGroups.map((group) => {
     const hasAvailable = availablePerformers.some((p) =>
@@ -66,13 +105,20 @@ export async function GET(req: NextRequest) {
   const skipped = await prisma.lead.count({
     where: {
       status: "pendding",
-    },
+      addedBy: {
+          in: hostessIds,
+      },
+  },
+    
   })
 
   const unresponsive = await prisma.lead.count({
     where: {
       status: {
         notIn: ["closed", "completed"],
+      },
+      addedBy: {
+          in: hostessIds,
       },
       updatedAt: {
         lt: new Date(now.getTime() - 24 * 60 * 60 * 1000),
